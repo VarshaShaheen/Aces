20e89cdcb5437e0804397729b7ee0815805be281
Varsha <varshashaheen2003@gmail.com>
2022-11-12 09:30:10 +0530

    background color changed

---
+++ script.js
@@ -0,0 +1,13 @@
+function show() {
+
+    document.getElementById('image')
+        .style.display = "block";
+
+
+    document.getElementById('btnID')
+        .style.display = "none";
+}
+function show_members(){
+    document.getElementById('members')
+        .style.display = "block";
+}
